7d0e68433caa8904b941a82d769a02d4b95b905f
liubenlopez <liuben.lopez@gmail.com>
2023-10-23 12:48:20 -0400

    delete images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { NgOptimizedImage } from '@angular/common';
+import { Product } from "./models/product.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'midudev-app';
+  title = 'Fake Store';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  ngOnInit(){
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
+
+
+
